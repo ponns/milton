041ee50f2fb0c049b727e0471144fa6859996d55
brad <brad@89edebda-9267-40c1-a934-9420f7305bf7>
2012-04-27 05:11:10 +0000

    fix for new constructor on ServletRequest

--- milton/milton-caldav-demo/src/main/java/com/ettrema/http/caldav/demo/servlet/CaldavMiltonServlet.java
+++ milton/milton-caldav-demo/src/main/java/com/ettrema/http/caldav/demo/servlet/CaldavMiltonServlet.java
@@ -29,68 +29,68 @@ import org.slf4j.LoggerFactory;
 
 /**
  * This is an example servlet to show how to use milton's caldav without spring
- * 
+ *
  * Currently this isnt used in this project.
- * 
- * Note that this is functionally equivalent to the spring xml configuration, just
- * uses simple substitution of xml constructs (eg <constructur-arg>) with plain
- * java equivalents.
+ *
+ * Note that this is functionally equivalent to the spring xml configuration,
+ * just uses simple substitution of xml constructs (eg <constructur-arg>) with
+ * plain java equivalents.
  *
  * @author brad
  */
 public class CaldavMiltonServlet implements Servlet {
 
-	private Logger log = LoggerFactory.getLogger(MiltonServlet.class);
-	private ServletConfig config;
-	protected HttpManager httpManager;
+    private Logger log = LoggerFactory.getLogger(MiltonServlet.class);
+    private ServletConfig config;
+    protected HttpManager httpManager;
 
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		TResourceFactory demoResourceFactory = new com.ettrema.http.caldav.demo.TResourceFactory();
-		WebDavResourceTypeHelper rth = new com.bradmcevoy.http.webdav.WebDavResourceTypeHelper();
-		CalendarResourceTypeHelper crth = new com.ettrema.http.caldav.CalendarResourceTypeHelper(
-				new com.ettrema.http.acl.AccessControlledResourceTypeHelper(rth));
-		AuthenticationService authService = new com.bradmcevoy.http.AuthenticationService();
-		HandlerHelper hh = new com.bradmcevoy.http.HandlerHelper(authService);
-		DefaultWebDavResponseHandler defaultResponseHandler = new com.bradmcevoy.http.webdav.DefaultWebDavResponseHandler(authService, crth);
-		Http11Protocol http11 = new com.bradmcevoy.http.http11.Http11Protocol(defaultResponseHandler, hh);
-		WebDavProtocol webdav = new com.bradmcevoy.http.webdav.WebDavProtocol(hh, crth, defaultResponseHandler, null);
-		CalDavProtocol caldav = new com.ettrema.http.caldav.CalDavProtocol(demoResourceFactory, defaultResponseHandler, hh, webdav);
-		ACLProtocol acl = new com.ettrema.http.acl.ACLProtocol(webdav);
-		ProtocolHandlers protocols = new com.bradmcevoy.http.ProtocolHandlers(Arrays.asList(http11, webdav, caldav, acl));
-		httpManager = new com.bradmcevoy.http.HttpManager(demoResourceFactory, defaultResponseHandler, protocols);
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        TResourceFactory demoResourceFactory = new com.ettrema.http.caldav.demo.TResourceFactory();
+        WebDavResourceTypeHelper rth = new com.bradmcevoy.http.webdav.WebDavResourceTypeHelper();
+        CalendarResourceTypeHelper crth = new com.ettrema.http.caldav.CalendarResourceTypeHelper(
+                new com.ettrema.http.acl.AccessControlledResourceTypeHelper(rth));
+        AuthenticationService authService = new com.bradmcevoy.http.AuthenticationService();
+        HandlerHelper hh = new com.bradmcevoy.http.HandlerHelper(authService);
+        DefaultWebDavResponseHandler defaultResponseHandler = new com.bradmcevoy.http.webdav.DefaultWebDavResponseHandler(authService, crth);
+        Http11Protocol http11 = new com.bradmcevoy.http.http11.Http11Protocol(defaultResponseHandler, hh);
+        WebDavProtocol webdav = new com.bradmcevoy.http.webdav.WebDavProtocol(hh, crth, defaultResponseHandler, null);
+        CalDavProtocol caldav = new com.ettrema.http.caldav.CalDavProtocol(demoResourceFactory, defaultResponseHandler, hh, webdav);
+        ACLProtocol acl = new com.ettrema.http.acl.ACLProtocol(webdav);
+        ProtocolHandlers protocols = new com.bradmcevoy.http.ProtocolHandlers(Arrays.asList(http11, webdav, caldav, acl));
+        httpManager = new com.bradmcevoy.http.HttpManager(demoResourceFactory, defaultResponseHandler, protocols);
 
-	}
+    }
 
-	@Override
-	public void destroy() {
-	}
+    @Override
+    public void destroy() {
+    }
 
-	@Override
-	public void service(javax.servlet.ServletRequest servletRequest, javax.servlet.ServletResponse servletResponse) throws ServletException, IOException {
-		HttpServletRequest req = (HttpServletRequest) servletRequest;
-		HttpServletResponse resp = (HttpServletResponse) servletResponse;
-		try {
-			Request request = new ServletRequest(req);
-			Response response = new ServletResponse(resp);
-			httpManager.process(request, response);
-		} finally {
-			try {
-				//servletResponse.getOutputStream().flush();
-				servletResponse.flushBuffer();
-			} catch (Exception e) {
-				log.warn("exception flushing, probably no real problem", e);
-			}
-		}
-	}
+    @Override
+    public void service(javax.servlet.ServletRequest servletRequest, javax.servlet.ServletResponse servletResponse) throws ServletException, IOException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        try {
+            Request request = new ServletRequest(req, config.getServletContext());
+            Response response = new ServletResponse(resp);
+            httpManager.process(request, response);
+        } finally {
+            try {
+                //servletResponse.getOutputStream().flush();
+                servletResponse.flushBuffer();
+            } catch (Exception e) {
+                log.warn("exception flushing, probably no real problem", e);
+            }
+        }
+    }
 
-	@Override
-	public String getServletInfo() {
-		return "CaldavMiltonServlet";
-	}
+    @Override
+    public String getServletInfo() {
+        return "CaldavMiltonServlet";
+    }
 
-	@Override
-	public ServletConfig getServletConfig() {
-		return config;
-	}
+    @Override
+    public ServletConfig getServletConfig() {
+        return config;
+    }
 }
