da67e7e90e952484b7800767867af07778f279af
brad <brad@bradslaptop>
2012-05-17 15:36:38 +1200

    added logging to help diagnose problems when multiple property sources are in use

--- milton/milton-api/src/main/java/com/bradmcevoy/http/webdav/PropertySourcesList.java
+++ milton/milton-api/src/main/java/com/bradmcevoy/http/webdav/PropertySourcesList.java
@@ -3,6 +3,8 @@ package com.bradmcevoy.http.webdav;
 import com.bradmcevoy.property.PropertySource;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -10,12 +12,15 @@ import java.util.List;
  */
 public class PropertySourcesList extends ArrayList<PropertySource> {
 
+	private static final Logger log = LoggerFactory.getLogger( PropertySourcesList.class );
+	
     private static final long serialVersionUID = 1L;
 
     /**
      * Constructs an empty list
      */
     public PropertySourcesList() {
+		log.info("Created PropertySourcesList");
     }
 
     /**
@@ -25,8 +30,17 @@ public class PropertySourcesList extends ArrayList<PropertySource> {
      */
     public PropertySourcesList( ResourceTypeHelper resourceTypeHelper ) {
         this.addAll( PropertySourceUtil.createDefaultSources( resourceTypeHelper ) );
+		log.info("Created propertysourceslist, hashcode: " + hashCode() + " size: " + size());		
     }
 
+	@Override
+	public boolean add(PropertySource e) {
+		log.info("adding property source: " + e.getClass() + " to PropertySourcesList: " + hashCode());
+		return super.add(e);
+	}
+
+	
+	
     /**
      * Allows you to add an extra source to the default list
      *
